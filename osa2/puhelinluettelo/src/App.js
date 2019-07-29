f4b57477fa51b55a68018b7c9c37fab57106afee
wiiwiih <wiiwiih@hotmail.com>
2019-07-29 13:17:16 +0300

    Puhelinluettelo steps 7-10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import phonebookService from './services/Phonebook'
 import PhonebookTable from './components/PhonebookTable'
 import FilterForm from './components/FilterForm'
 import PersonForm from './components/PersonForm'
@@ -11,10 +11,10 @@ const App = () => {
   const [ filterText, setFilterText ] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    phonebookService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -33,18 +33,48 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
 
-    if (persons.some((person) => (person.name.toLowerCase() === newName.toLowerCase()))) {
-      window.alert(`${newName} is already added to phonebook`)
+    if (persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())) {
+      if (window.confirm(`${newName} is already added to phonebook. Replace the old number with a new one?`)) {
+        const person = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
+        const changedPerson = { ...person, number: newNumber}
+
+        phonebookService
+          .update(changedPerson.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== changedPerson.id ? person : returnedPerson))
+          })
+          .catch(error => {
+            alert(`${changedPerson.name} was already deleted from the server`)
+            setPersons(persons.filter(p => p.id !== changedPerson.id))
+          })
+      }
     } else {
       const newPerson = {
         name: newName,
-        number: newNumber,
-        id: newName
+        number: newNumber
       }
 
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      phonebookService
+        .create(newPerson)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+      
+    }
+  }
+
+  const deletePerson = (selectedPerson) => {
+    if (window.confirm(`Delete ${selectedPerson.name}?`)) {
+      phonebookService
+        .remove(selectedPerson.id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== selectedPerson.id))
+          alert(
+            `${selectedPerson.name} was deleted`
+          )
+        })
     }
   }
 
@@ -55,7 +85,7 @@ const App = () => {
       <h2>Add new person</h2>
       <PersonForm name={newName} onNameChange={HandleNameChange} number={newNumber} onNumberChange={HandleNumberChange} onSubmit={addPerson} />
       <h2>Numbers</h2>
-      <PhonebookTable persons={persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase()))} />
+      <PhonebookTable persons={persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase()))} onDelete={deletePerson} />
     </div>
   )
 
--- osa2/puhelinluettelo/src/components/PhonebookTable.js
+++ osa2/puhelinluettelo/src/components/PhonebookTable.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const PhonebookTable = ({persons}) => (
+const PhonebookTable = ({persons, onDelete}) => (
   <table>
       <tbody>
           {persons.map(person => <tr key={person.name}>
                                     <td>{person.name}</td>
                                     <td>{person.number}</td>
+                                    <td><button onClick={() => onDelete(person)}>Delete</button></td>
                                  </tr>)}
       </tbody>
   </table>
